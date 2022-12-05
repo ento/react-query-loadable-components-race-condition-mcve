bb389ebbf21c310ddc99681b68487fe0b31d93f2
ento <ento+github@i.pearlwaffles.xyz>
2022-12-05 10:57:47 -0800

    Simplify hooks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,63 +2,8 @@ import React, { useEffect } from "react";
 
 import { useProjects } from "../queries";
 
-const delayPerHookMsec = 20;
-
-function block(minWaitMillisecs) {
-  const start = new Date().getTime();
-  while (true) {
-    const now = new Date().getTime();
-    if (now - start > minWaitMillisecs) break;
-  }
-}
-
-function useProjectsNestedNestedNested() {
-  const { data } = useProjects();
-  useEffect(() => {
-    block(delayPerHookMsec);
-    console.log(
-      new Date().getTime(),
-      "useProjectsNestedNestedNested",
-      "useEffect callback",
-      data
-    );
-  }, [data]);
-
-  return { data };
-}
-
-function useProjectsNestedNested() {
-  const { data } = useProjectsNestedNestedNested();
-  useEffect(() => {
-    block(delayPerHookMsec);
-    console.log(
-      new Date().getTime(),
-      "useProjectsBranch",
-      "useEffect callback",
-      data
-    );
-  }, [data]);
-  return { data };
-}
-
-function useProjectsNested() {
-  const { data } = useProjectsNestedNested();
-  useEffect(() => {
-    block(delayPerHookMsec);
-    console.log(
-      new Date().getTime(),
-      "useProjectsNested",
-      "useEffect callback",
-      data
-    );
-  }, [data]);
-
-  return { data };
-}
-
 export default function Projects() {
-  block(delayPerHookMsec);
   console.log(new Date().getTime(), "Rendering Projects");
-  const { data } = useProjectsNested();
+  const { data } = useProjects();
   return <>projects: {JSON.stringify(data)}</>;
 }
