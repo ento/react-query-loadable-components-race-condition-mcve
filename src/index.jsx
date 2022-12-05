53f989c0cf61c98af2ff6953b81197dde4653c22
ento <ento+github@i.pearlwaffles.xyz>
2022-12-05 10:47:43 -0800

    Upgrade to React v18

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, {useReducer, useEffect} from "react";
-import { render } from "react-dom";
-// import { createRoot } from "react-dom/client";
+// import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import loadableComponent from "@loadable/component";
 
@@ -65,10 +65,10 @@ function Page() {
 }
 
 // React < 18
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// render(<App />, rootElement);
 
 // React 18+
-// const container = document.getElementById("root");
-// const root = createRoot(container); // createRoot(container!) if you use TypeScript
-// root.render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
